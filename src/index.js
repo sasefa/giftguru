497fef792d2ee786b1ac9d3cee315945c7bc8daa
sasefa <131699944+sasefa@users.noreply.github.com>
2023-10-14 01:44:42 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 if (process.env.NODE_ENV === 'production') disableReactDevTools();
-const app = require('../../server/server');  // Importing the configured Express app
+const app = require('src/App.js');  // Importing the configured Express app
 
 const PORT = process.env.PORT || 5000;
 
